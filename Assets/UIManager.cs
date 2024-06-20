6e65abd2af1c6b9742a99efad928c5fa2884e555
DrinkLemonade <117670511+DrinkLemonade@users.noreply.github.com>
2024-06-20 18:49:11 +0200

    Login UI/logic done, tween
    -Finished login UI
    -Added login logic
    -Added tween animation upon login
    -Removed Steampunk UI Utils (didn't compile on mobile)

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,30 +1,123 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mail;
+using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
 {
-    VisualElement root;
-    TextField emailField;
+    VisualElement root, loginScreen;
+    TextField emailField, passwordField;
     Button button;
+    Label emailWarning, passwordWarning;
+
+    bool validated = false;
+    float validateSlide = 0;
+    [SerializeField]
+    Ease easeType;
+    [SerializeField]
+    AnimationCurve easeCurve;
+    [SerializeField]
+    float easeTimeSeconds = 2f;
+    [SerializeField]
+    bool useCurveInsteadOfEase = false;
 
     [SerializeField]
     GameObject UIDoc;
     public static Action OnButtonClicked;
 
+    [SerializeField]
+    string shownIfEmpty = "AAAAAAAAAAAAAH";
+
     // Start is called before the first frame update
     void Start()
     {
+        //Get
         root = UIDoc.GetComponent<UIDocument>().rootVisualElement;
+        loginScreen = root.Q<VisualElement>("Wood");
         emailField = root.Q<TextField>("EmailField");
+        passwordField = root.Q<TextField>("PasswordField");
+
+        emailWarning = root.Q<Label>("EmailWarningLabel");
+        passwordWarning = root.Q<Label>("PasswordWarningLabel");
+        emailWarning.visible = false;
+        passwordWarning.visible = false;
+
+        //Email
+        emailField.RegisterCallback<ChangeEvent<string>, TextField>(OnFieldStringChangedEvent, emailField);
+        emailField.RegisterCallback<ClickEvent, TextField>(OnFieldClickEvent, emailField);
+        SetEmptyDefaultText(emailField, true);
+
+        //Password
+        passwordField.RegisterCallback<ChangeEvent<string>, TextField>(OnFieldStringChangedEvent, passwordField);
+        passwordField.RegisterCallback<ClickEvent, TextField>(OnFieldClickEvent, passwordField);
+        SetEmptyDefaultText(passwordField, true);
+
+        //Login button
         button = root.Q<Button>("Button");
-        button.clickable.clicked += OnClick; //TODO: Don't allow it to stack
+        button.clickable.clicked += OnButtonClick; //TODO: Don't allow it to stack
         Debug.Log(button.name);
     }
 
-    void OnClick()
+    void SetEmptyDefaultText(TextField field, bool active)
     {
+        Debug.Log($"running SetEmpty for {field.name}, active: {active}");
+        field.style.unityFontStyleAndWeight = active ? FontStyle.Italic : FontStyle.Normal;
+        field.SetValueWithoutNotify(active ? shownIfEmpty : "");
+    }
+
+    void OnFieldClickEvent(ClickEvent ev, TextField field)
+    {
+        if (field.text == shownIfEmpty) SetEmptyDefaultText(field, false);
+        if (field.name == "PasswordField") field.isPasswordField = true;
+    }
+    void OnFieldStringChangedEvent(ChangeEvent<string> ev, TextField field)
+    {
+        if (ev.newValue == "") SetEmptyDefaultText(field, false);
+        //TODO: For some reason this isn't working but it wasn't really requested, soooo
+        else field.style.unityFontStyleAndWeight = FontStyle.Normal;
+    }
+
+    void OnButtonClick()
+    {
+        if (validated) return;
+
+        Debug.Log($"button clicked! Contents are: {emailField.text}, {passwordField.text}");
+        bool okMail = false, okPassword = false;
+        okMail = (emailField.value != "");
+        if (okMail) okMail = EmailIsValid(emailField.value);
+        okPassword = (passwordField.value != "" && passwordField.value != shownIfEmpty);
+        emailWarning.visible = !okMail;
+        passwordWarning.visible = !okPassword;
+        if (okMail && okPassword) ValidateLogin();
+    }
+
+    void ValidateLogin()
+    {
+        if (easeCurve != null && useCurveInsteadOfEase) DOTween.To(() => validateSlide, x => validateSlide = x, -110, easeTimeSeconds).SetEase(easeCurve);
+        else DOTween.To(() => validateSlide, x => validateSlide = x, -110, easeTimeSeconds).SetEase(easeType);
+    }
+
+    private void Update()
+    {
+        loginScreen.style.top = Length.Percent(10 + validateSlide);
+    }
+
+    public bool EmailIsValid(string emailaddress)
+    {
+        try
+        {
+            MailAddress m = new MailAddress(emailaddress);
+
+            return true;
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
     }
 }
