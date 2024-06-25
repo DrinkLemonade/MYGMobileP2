418270e85c243b90c9071cb8d9f035e924ebe1e6
DrinkLemonade <117670511+DrinkLemonade@users.noreply.github.com>
2024-06-25 11:06:02 +0200

    Player list
    -Added placeholder player lobby list + example player character SOs
    -Renamed files
    -Changed VisualElements: frame and background for login name/password fields to correct images

---
+++ Assets/PlayerListEntryController.cs
@@ -0,0 +1,22 @@
+using UnityEngine.UIElements;
+
+public class PlayerListEntryController
+{
+    Label m_NameLabel;
+
+    // This function retrieves a reference to the 
+    // Player name label inside the UI element.
+    public void SetVisualElement(VisualElement visualElement)
+    {
+        m_NameLabel = visualElement.Q<Label>("character-name");
+    }
+
+    // This function receives the Player whose name this list 
+    // element is supposed to display. Since the elements list 
+    // in a `ListView` are pooled and reused, it's necessary to 
+    // have a `Set` function to change which Player's data to display.
+    public void SetPlayerData(CharacterData PlayerData)
+    {
+        m_NameLabel.text = PlayerData.CharacterName;
+    }
+}
